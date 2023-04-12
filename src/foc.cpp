9a1a64d8481d628ff49442dafbf8d08b1ff8d35d
keptsecret <sorchon@gmail.com>
2023-04-12 15:18:56 -0400

    two scenes

--- src/foc.cpp
+++ src/foc.cpp
@@ -3,7 +3,9 @@
 #include "fluidsim.h"
 
 int main() {
-    foc::FluidSimulation fluidsim(32, 32, 32, 0.25);
+
+	// Fluid sphere
+	foc::FluidSimulation fluidsim(64, 64, 64, 0.125);
 
 	double width, height, depth;
 	fluidsim.getSimulationDimensions(&width, &height, &depth);
@@ -12,6 +14,29 @@ int main() {
 
 	fluidsim.initialize();
 	fluidsim.simulate(30.0, 60);
+
+	// Fluid wave with obstacle
+	/*
+	foc::FluidSimulation fluidsim(128, 64, 64, 0.125);
+
+	double width, height, depth;
+	fluidsim.getSimulationDimensions(&width, &height, &depth);
+	fluidsim.addFluidCuboid(0, 0, 0, depth * 0.75, height * 0.5, depth);
+	fluidsim.addBodyForce(0, -25, 0);
+
+	std::vector<foc::Point3i> solidBlock;
+	for (int k = 16; k < 48; k++) {
+		for (int j = 0; j < 16; j++) {
+			for (int i = 84; i < 100; i++) {
+				solidBlock.push_back(foc::Point3i(i, j, k));
+			}
+		}
+	}
+	fluidsim.addSolidCells(solidBlock);
+
+	fluidsim.initialize();
+	fluidsim.simulate(30.0, 120);
+	 */
 
     return 0;
 }
